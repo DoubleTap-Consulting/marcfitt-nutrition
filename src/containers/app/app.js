b906e6cfdeb55e37aa7035df18943ab5173cbb92
Michael Mitrakos <mike.mitrakos@verys.com>
2017-07-15 02:55:30 -0700

    [feature]

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -9,10 +9,15 @@ const asyncCalculator = asyncLoader(() => require('../calculator/calculator'));
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Switch>
-          <Route path="/calculator" component={asyncCalculator} />
-        </Switch>
+      <div className="app">
+        {/*<Header />*/}
+        <div className="app-container row">
+          <Switch>
+            {/*<Route path="/" component={asyncLanding} />*/}
+            <Route path="/calculator" component={asyncCalculator} />
+          </Switch>
+        </div>
+        {/*<Footer />*/}
       </div>
     );
   }
---
+++ src/containers/calculator/steps/metrics/metrics.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './metrics.css';
+
+const Metrics = () => (
+  <div className="row align-middle align-justify">
+
+  </div>
+);
+
+export default Metrics;
