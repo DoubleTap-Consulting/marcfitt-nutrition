74c7ea0035c9006aa923d418c4d52ae3a48d9443
michaelaflores <mike@michaelflores.io>
2017-07-14 18:22:45 -0700

    Header updates

--- src/containers/header/header.js
+++ src/containers/header/header.js
@@ -3,8 +3,8 @@ import marcFittLogo from '../../images/logo.png';
 import './header.css';
 
 const Header = () => (
-  <div className="row container-header">
-    <img className="header-logo" src={marcFittLogo} height="30" width="202" />
+  <div className="row align-middle align-justify header-container">
+    <img className="header-logo" src={marcFittLogo} />
   </div>
 );
 
