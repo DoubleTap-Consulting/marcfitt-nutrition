6cc787f063f2674c1e1faf5d5711978e951e5b25
michaelaflores <mike@michaelflores.io>
2017-07-14 17:47:24 -0700

    Landing routing
    - Number of setup changes to app
    - Routing for landing page

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
+import { Route, Switch } from 'react-router';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+// import Header from '../header/header';
+// import Footer from '../footer/footer';
+import asyncLoader from '../../shared-components/asyncComponentLoader';
 import './app.css';
 
+const asyncLanding = asyncLoader(() => require('../../containers/landing/landing'));
+
 class App extends Component {
+  static get propTypes() {
+    return {};
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        {/*<Header />*/}
+        <div className="app-container">
+          <Switch>
+            <Route path="/" component={asyncLanding} />
+          </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*<Footer />*/}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+};
+
+export default connect(mapStateToProps)(App);
