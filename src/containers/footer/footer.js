fc9631e4122ea1de7588f2a2c208c3c869a9d681
Michael Mitrakos <mike.mitrakos@verys.com>
2017-07-16 00:28:10 -0700

    [feature] add footer

---
+++ src/containers/footer/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => (
+  <div className="row align-middle align-justify footer-container">
+    
+  </div>
+);
+
+export default Footer;
