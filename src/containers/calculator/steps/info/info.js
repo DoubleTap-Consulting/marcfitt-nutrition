60be581c22cefd61eb8d5ac6e159dbd13e3dc835
Michael Mitrakos <mike.mitrakos@verys.com>
2017-07-15 20:05:07 -0700

    [fix]

--- src/containers/calculator/steps/info/info.js
+++ src/containers/calculator/steps/info/info.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 import './info.css';
 
-const Info = () => (
-  <div className="row align-middle align-justify">
-
+const Info = ({ gender, handleChange }) => (
+  <div className="align-middle align-justify">
+    <h1>Info</h1>
+    <TextField
+      hintText="Age"
+    />
+    <SelectField
+      floatingLabelText="Gender"
+      value={gender}
+      onChange={handleChange}
+    >
+      <MenuItem value={1} primaryText="Male" />
+      <MenuItem value={2} primaryText="Female" />
+    </SelectField>
   </div>
 );
 
