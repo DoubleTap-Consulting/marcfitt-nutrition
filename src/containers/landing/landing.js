8252e18938ca882f820dbe659546896272a2bd53
michaelaflores <mike@michaelflores.io>
2017-07-15 19:06:49 -0700

    UI update
    - Added header
    - centered button

--- src/containers/landing/landing.js
+++ src/containers/landing/landing.js
@@ -9,21 +9,24 @@ class Landing extends Component {
     return (
       <MuiThemeProvider>
         <div className="columns small-12 landing-container">
+          <div className="row align-center text-center">
+            <div className="columns small-12 medium-10 large-6">
+              <h1 className="app-title">Nutrition Planner</h1>
+            </div>
+          </div>
           <div className="row align-center">
             <div className="columns small-12 medium-10 large-6">
               <p className="landing-paragraph">The Nutrition Planner is Marc’s revolutionary tool that will allow you to plan your own diet, so you can choose the foods that are most convenient, and taste best to you!</p>
               <p className="landing-paragraph-sub1">You’ll have the ability to calculate your caloric needs with ease, thanks to the built in calorie calculator. Enter your height, weight, goal, and smoothly transition these stats into the Planner so you can begin building your diet.</p>
-              <div className="row align-center landing-start">
-                <div className="small-8 columns">
-                  <RaisedButton
-                    buttonStyle={buttonStyles.buttonStyle}
-                    labelStyle={buttonStyles.buttonLabelStyle}
-                    label="Get Started"
-                    secondary={false}
-                    onTouchTap={this.startTap}
-                  />
-                </div>
-              </div>
+            </div>
+            <div className="columns small-12 medium-10 large-6 text-center">
+              <RaisedButton
+                buttonStyle={buttonStyles.buttonStyle}
+                labelStyle={buttonStyles.buttonLabelStyle}
+                label="Get Started"
+                secondary={false}
+                onTouchTap={this.startTap}
+              />
             </div>
           </div>
         </div>
