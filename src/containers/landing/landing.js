eeca6d6912bc2ab3cd9ccddd7f7e76d1a73b1933
michaelaflores <mike@michaelflores.io>
2017-08-13 14:37:52 -0700

    [feature] rename the app, verbiage change

--- src/containers/landing/landing.js
+++ src/containers/landing/landing.js
@@ -12,12 +12,12 @@ class Landing extends Component {
         <div className="columns small-12 landing-container">
           <div className="row align-center text-center">
             <div className="columns small-12">
-              <h1 className="app-title">Nutrition Planner</h1>
+              <h1 className="app-title">Calorie Calculator</h1>
             </div>
           </div>
           <div className="row align-center">
             <div className="columns small-8">
-              <p className="landing-paragraph">The Nutrition Planner is Marc’s revolutionary program that will give you the tools to reach your fitness goals through a personalized breakdown of your macronutrient and caloric needs.</p>
+              <p className="landing-paragraph">The Calorie Calculator is Marc’s revolutionary tool that will help you reach your fitness goals through a personalized breakdown of your macronutrient and caloric needs.</p>
               <p className="landing-paragraph sub1">You’ll have the ability to calculate your caloric needs with ease, thanks to the built-in calorie calculator. Enter a few personal statistics and you'll be given all the information you need to begin building your diet.</p>
             </div>
             <div className="columns small-12 text-center">
