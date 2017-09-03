ac6ea2f40539f8c0474b06a19e46a3a3c41eef58
Michael Mitrakos <mike.mitrakos@verys.com>
2017-09-02 22:25:58 -0700

    [fix]

--- server/controllers/email_list.js
+++ server/controllers/email_list.js
@@ -5,83 +5,85 @@ const MD5 = require('crypto-js/md5');
 emailListController.addToRiseEmailList = (req, res) => {
   console.log('------------')
   console.log('Performing: add to email list: ', req.body.email_address)
+  console.log('req -----------', req);
   if (!req.body.email_address) {
     res.status(400).send('No email provided')
+  } else {
+    const hash = MD5(req.body.email_address.toLowerCase());
+    let getStatus = 'Added';
+    const getConfig = {
+      method: 'GET',
+      uri: process.env.RISE_URL + '/' + hash.toString(),
+      headers: {
+        'Authorization': process.env.RISE_API
+      },
+      json: true
+    }
+  
+    const putConfig = {
+      method: 'PUT',
+      uri: process.env.RISE_URL + '/' + hash.toString(),
+      body: {
+        "email_address": req.body.email_address,
+        "status_if_new": "subscribed",
+        "status": "subscribed",
+        "merge_fields": {
+          "BIRTHDAY": req.body.birthday,
+          "GENDER": req.body.gender
+        }
+      },
+      headers: {
+        'Authorization': process.env.RISE_API
+      },
+      json: true
+    }
+  
+    request(getConfig)
+      .then((status) => {
+        if (status.status === 'subscribed') {
+          getStatus = 'Updated'
+        }
+        console.log('Found contact with status: ', status.status)
+  
+        request(putConfig)
+          .then((status) => {
+            console.log('Successfully updated contact')
+            console.log('------------')
+            res.status(200).send({
+              message: getStatus,
+              response: status
+            })
+          })
+          .catch((err) => {
+            console.log('err', err.message)
+            console.log('------------')
+            res.status(400).send({
+              message: 'Failed to add or update',
+              error: err
+            })
+          });
+      })
+      .catch((err) => {
+        console.log('getError', err.message)
+        request(putConfig)
+          .then((status) => {
+            console.log('Successfully added contact')
+            console.log('------------')
+            res.status(200).send({
+              message: getStatus,
+              response: status
+            })
+          })
+          .catch((err) => {
+            console.log('err', err.message)
+            console.log('------------')
+            res.status(400).send({
+              message: 'Failed to add or update',
+              error: err
+            })
+          });
+      });
   }
-  const hash = MD5(req.body.email_address.toLowerCase());
-  let getStatus = 'Added';
-  const getConfig = {
-    method: 'GET',
-    uri: process.env.RISE_URL + '/' + hash.toString(),
-    headers: {
-      'Authorization': process.env.RISE_API
-    },
-    json: true
-  }
-
-  const putConfig = {
-    method: 'PUT',
-    uri: process.env.RISE_URL + '/' + hash.toString(),
-    body: {
-      "email_address": req.body.email_address,
-      "status_if_new": "subscribed",
-      "status": "subscribed",
-      "merge_fields": {
-        "BIRTHDAY": req.body.birthday,
-        "GENDER": req.body.gender
-      }
-    },
-    headers: {
-      'Authorization': process.env.RISE_API
-    },
-    json: true
-  }
-
-  request(getConfig)
-    .then((status) => {
-      if (status.status === 'subscribed') {
-        getStatus = 'Updated'
-      }
-      console.log('Found contact with status: ', status.status)
-
-      request(putConfig)
-        .then((status) => {
-          console.log('Successfully updated contact')
-          console.log('------------')
-          res.status(200).send({
-            message: getStatus,
-            response: status
-          })
-        })
-        .catch((err) => {
-          console.log('err', err.message)
-          console.log('------------')
-          res.status(400).send({
-            message: 'Failed to add or update',
-            error: err
-          })
-        });
-    })
-    .catch((err) => {
-      console.log('getError', err.message)
-      request(putConfig)
-        .then((status) => {
-          console.log('Successfully added contact')
-          console.log('------------')
-          res.status(200).send({
-            message: getStatus,
-            response: status
-          })
-        })
-        .catch((err) => {
-          console.log('err', err.message)
-          console.log('------------')
-          res.status(400).send({
-            message: 'Failed to add or update',
-            error: err
-          })
-        });
-    });
 }
 
 module.exports = emailListController
